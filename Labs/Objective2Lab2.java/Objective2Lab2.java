a7d5faefcca25bc859ad35b4d6d95c7ee517cf14
James Taft <jamestaft@Jamess-MacBook-Pro.local>
2022-08-11 12:26:33 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Jim Taft";
+    int age = 42;
+    String state = "Colorado";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
