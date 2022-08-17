9ff9888b546acd4fcacc0beb69899ec7f13c6da0
Jim Taft <jamesrtaft@gmail.com>
2022-08-17 12:28:57 -0600

    complete objective2lab3

---
+++ Labs/Objective2/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+  int yearBorn = 1979;
+  String monthBorn = "October";
+  boolean todayWeekend = false;
+  System.out.println(yearBorn);
+  System.out.println(monthBorn);
+  System.out.println(todayWeekend);
+  }
+}
